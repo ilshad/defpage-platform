d3113bf4259e3bebc126345c909e2b97603dbdc7
Ilshad Khabibullin <astoon@spacta.com>
2012-03-01 04:16:15 +0400

    get sources and meta

--- include/platform.hrl
+++ include/platform.hrl
@@ -3,5 +3,4 @@
 -define(META_URL, "http://localhost:9005").
 -define(GD_URL, "http://localhost:9020").
 -define(SYSTEM_USER, "system").
--define(META_AUTH,
-	{"Authorization", "Basic " ++ binary_to_list(base64:encode(?SYSTEM_USER ++ ":1"))}).
+-define(META_AUTH, {"Authorization", "Basic " ++ binary_to_list(base64:encode(?SYSTEM_USER ++ ":1"))}).
--- src/platform_source.erl
+++ src/platform_source.erl
@@ -2,31 +2,67 @@
 %% @doc Synchronize documents in metadata server with sources.
 
 -module(platform_source).
--export([sync/1, google_docs/1]).
+-export([sync/1]).
+-export([get_meta/1, get_sources/2]).
 
 -include("platform.hrl").
 
--record(document, {id::string(), title::string(), modified::string()}).
+-type(source_type() :: gd | undefined).
 
--spec(sync(Id::integer()) -> ok).
+-record(document, {id :: string(),
+		   title :: string(),
+		   modified :: string(),
+		   source :: [tuple()] % proplist
+		  }).
 
-%% @doc Run sync process.
-sync(CollectionId) -> CollectionId.
+-spec(sync(Id::integer()) -> tuple()).
+sync(CollectionId) ->
+    {SourceType, MetaDocs} = get_meta(CollectionId),
+    SourceDocs = get_sources(SourceType, CollectionId),
+    {MetaDocs, SourceDocs}.
 
--spec(google_docs(CollectionId::integer()) -> [#document{}]).
+-spec(get_meta(CollectionId::integer()) -> {source_type(), [#document{}]}).
+get_meta(CollectionId) ->
+    Url = ?META_URL ++ "/collections/" ++ integer_to_list(CollectionId),
+    case httpc:request(get, {Url, [?META_AUTH]}, [], []) of
+	{ok, {{_, 200, _}, _, Body}} ->
+	    {struct, Fields} = mochijson:decode(Body),
+	    {array, Docs} = proplists:get_value("documents", Fields),
+	    {source_type(Fields), [json_to_document(meta, X) || X <- Docs]};
+	{ok, {{_, 404, _}, _, _}} ->
+	    error;
+	_ ->
+	    error
+    end.
 
-%% @doc Get google docs for given collection. Argument is collection Id.
-google_docs(CollectionId) ->
+-spec(source_type(Fields::[tuple()]) -> source_type()).
+source_type(Fields) ->
+    {array, [{struct, Source} | _]} = proplists:get_value("sources", Fields), % assume alone
+    case proplists:get_value("type", Source) of
+	"gd" -> gd;
+	_ -> error
+    end.
+
+-spec(get_sources(source_type(), CollectionId::integer()) -> [#document{}]).
+get_sources(gd, CollectionId) ->
     Url = ?GD_URL ++ "/api/collection/" ++ integer_to_list(CollectionId) ++ "/documents",
     case httpc:request(Url) of
 	{ok, {{_, 200, _}, _, Body}} ->
 	    {array, Docs} = mochijson:decode(Body),
-	    [json_to_document(X) || X <- Docs];
-	{ok, {{_, 404, _}, _, _}} -> error;
-	_ -> error
+	    [json_to_document(source, X) || X <- Docs];
+	{ok, {{_, 404, _}, _, _}} ->
+	    error;
+	_ ->
+	    error
     end.
 
-json_to_document({struct, Fields}) ->
+json_to_document(Fields) ->
     #document{id = proplists:get_value("id", Fields),
 	      title = proplists:get_value("title", Fields),
 	      modified = rfc3339:parse_epoch(proplists:get_value("modified", Fields))}.
+
+json_to_document(source, {struct, Fields}) ->
+    json_to_document(Fields);
+json_to_document(meta, {struct, Fields}) ->
+    BaseRecord = json_to_document(Fields),
+    BaseRecord#document{source = proplists:get_value("source", Fields)}.
