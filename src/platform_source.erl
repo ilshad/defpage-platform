d15a8dce151cdad485a34d531fbf04d4bdb44526
Ilshad Khabibullin <astoon@spacta.com>
2012-03-01 12:19:52 +0400

    used mochijson2

--- src/platform_source.erl
+++ src/platform_source.erl
@@ -22,7 +22,8 @@
 sync(CollectionId) ->
     {SourceType, MetaDocs} = get_meta(CollectionId),
     SourceDocs = get_sources(SourceType, CollectionId),
-    ok.
+    %lists:foreach(get_fun_update(MetaDocs), SourceDocs),
+    {MetaDocs, SourceDocs}.
 
 -spec(get_meta(CollectionId::integer()) -> {source_type(), [#meta_doc{}]}).
 %% Get metadata info.
@@ -30,8 +31,8 @@ get_meta(CollectionId) ->
     Url = ?META_URL ++ "/collections/" ++ integer_to_list(CollectionId),
     case httpc:request(get, {Url, [?META_AUTH]}, [], []) of
 	{ok, {{_, 200, _}, _, Body}} ->
-	    {struct, Fields} = mochijson:decode(Body),
-	    {array, Docs} = proplists:get_value("documents", Fields),
+	    {struct, Fields} = mochijson2:decode(Body),
+	    Docs = proplists:get_value(<<"documents">>, Fields),
 	    {source_type(Fields), [meta_doc(X) || X <- Docs]};
 	{ok, {{_, 404, _}, _, _}} ->
 	    error;
@@ -42,9 +43,9 @@ get_meta(CollectionId) ->
 -spec(source_type(Fields::[tuple()]) -> source_type()).
 %% Extract source type for collection. Assume alone.
 source_type(Fields) ->
-    {array, [{struct, Source} | _]} = proplists:get_value("sources", Fields),
-    case proplists:get_value("type", Source) of
-	"gd" -> gd;
+    [{struct, Source} | _] = proplists:get_value(<<"sources">>, Fields),
+    case proplists:get_value(<<"type">>, Source) of
+	<<"gd">> -> gd;
 	_ -> error
     end.
 
@@ -54,23 +55,38 @@ get_sources(gd, CollectionId) ->
     Url = ?GD_URL ++ "/api/collection/" ++ integer_to_list(CollectionId) ++ "/documents",
     case httpc:request(Url) of
 	{ok, {{_, 200, _}, _, Body}} ->
-	    {array, Docs} = mochijson:decode(Body),
-	    [source_doc(X) || X <- Docs];
+	    [source_doc(X) || X <- mochijson2:decode(Body)];
 	{ok, {{_, 404, _}, _, _}} ->
 	    error;
 	_ ->
 	    error
     end.
 
+%% Create property with record #meta_doc{} from json structure
 meta_doc({struct, Fields}) ->
-    {struct, Source} = proplists:get_value("source", Fields),
-    {proplists:get_value("id", Source),
-     #meta_doc{meta_id = proplists:get_value("id", Fields),
-	       source_type = proplists:get_value("type", Source),
-	       title = proplists:get_value("title", Fields),
-	       modified = rfc3339:parse_epoch(proplists:get_value("modified", Fields))}}.
+    {struct, Source} = proplists:get_value(<<"source">>, Fields),
+    {proplists:get_value(<<"id">>, Source),
+     #meta_doc{meta_id = proplists:get_value(<<"id">>, Fields),
+	       source_type = proplists:get_value(<<"type">>, Source),
+	       title = proplists:get_value(<<"title">>, Fields),
+	       modified = rfc3339:parse_epoch(
+			    binary_to_list(
+			      proplists:get_value(<<"modified">>, Fields)))}}.
 
+%% Create property with record #source_doc{} from json structure
 source_doc({struct, Fields}) ->
-    {proplists:get_value("id", Fields),
-     #source_doc{title = proplists:get_value("title", Fields),
-		 modified = rfc3339:parse_epoch(proplists:get_value("modified", Fields))}}.
+    {proplists:get_value(<<"id">>, Fields),
+     #source_doc{title = proplists:get_value(<<"title">>, Fields),
+		 modified = rfc3339:parse_epoch(
+			      binary_to_list(
+				proplists:get_value(<<"modified">>, Fields)))}}.
+
+%-spec(get_fun_update(MetaDocs::list()) -> function(SourceDoc::tuple()) -> void()).
+%% Return function which update meta docs if need, for given source document.
+%get_fun_update(MetaDocs) ->
+%    fun({SourceId, _SourceDoc}) ->
+%	    case proplists:get_value(SourceId, MetaDocs) of
+%		undefined ->
+%		    ok
+%	    end
+%    end.
