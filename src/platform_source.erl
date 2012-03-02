98b9718fb8a423b1cf04cb03c8f13e339c973ef7
Ilshad Khabibullin <astoon@spacta.com>
2012-03-02 04:10:32 +0400

    create and update functions

--- src/platform_source.erl
+++ src/platform_source.erl
@@ -85,28 +85,50 @@ source_doc({struct, Fields}) ->
 		     CollectionId::integer(),
 		     MetaDocs::list()) -> function()).
 %% Return function which update meta docs if need, for given source document.
-get_fun_update(SourceType, CollectionId, MetaDocs) ->
+%% Check meta for corresponding document by given source.
+%% If it does not found, then create new document.
+%% If there is a corresponding document, then check `modified` and `title`
+%% and update if need.
+get_fun_update(SourceTypeAtom, CollectionId, MetaDocs) ->
+    SourceType = list_to_binary(atom_to_list(SourceTypeAtom)),
     fun({SourceId, SourceDoc}) ->
 	    case proplists:get_value(SourceId, MetaDocs) of
 		undefined ->
-		    Source = {struct, [{<<"type">>, list_to_binary(atom_to_list(SourceType))},
-				       {<<"id">>, SourceId}]},
-		    Fields = {struct, [{<<"title">>, SourceDoc#source_doc.title},
-				       {<<"source">>, Source},
-				       {<<"collection_id">>, CollectionId}]},
-		    Request = {?META_URL ++ "/documents/",
-			       [?META_AUTH],
-			       "application/json",
-			       iolist_to_binary(mochijson2:encode(Fields))},
-		    case httpc:request(post, Request, [], []) of
-			{ok, {{_, 201, _}, _, Body}} ->
-			    {struct, ResponseFields} = mochijson2:decode(Body),
-			    _DocId = proplists:get_value(<<"id">>, ResponseFields),
-			    ok;
-			_ ->
-			    ok
-		    end;
-		{meta_doc, _MMetaId, _SourceType, MTitle, _MModified} ->
-		    io:format("QQQQQQQQQQQQQ -- ~p -- QQQQQQQQQQQ", [MTitle])
+		    create_doc(CollectionId, SourceType, SourceId, SourceDoc);
+		{meta_doc, MetaId, SourceType, MetaTitle, MetaModified} ->
+		    update_doc(MetaId, MetaTitle, MetaModified, SourceDoc)
 	    end
     end.
+
+-spec(create_doc(CollectionId::integer(),
+		 SourceType::atom(),
+		 SourceId::string(),
+		 SourceDoc::term()) -> ok).
+%% Create corresponding document in the metadata server for given source document.
+create_doc(CollectionId, SourceType, SourceId, SourceDoc) ->
+    Fields = {struct, [{<<"title">>, SourceDoc#source_doc.title},
+		       {<<"source">>, {struct, [{<<"type">>, SourceType},
+						{<<"id">>, SourceId}]}},
+		       {<<"collection_id">>, CollectionId}]},
+
+    Request = {?META_URL ++ "/documents/",
+	       [?META_AUTH],
+	       "application/json",
+	       iolist_to_binary(mochijson2:encode(Fields))},
+
+    case httpc:request(post, Request, [], []) of
+	{ok, {{_, 201, _}, _, Body}} ->
+	    {struct, ResponseFields} = mochijson2:decode(Body),
+	    _DocId = proplists:get_value(<<"id">>, ResponseFields),
+	    ok;
+	_ ->
+	    ok
+    end.
+
+-spec(update_doc(MetaId::string(),
+		 MetaTitle::string(),
+		 MetaModified::integer(),
+		 SourceDoc::term()) -> ok).
+%% Update corresponding meta document if `title` or `modified` is updated.
+update_doc(MetaId, MetaTitle, MetaModified, SourceDoc) ->    
+    {MetaId, MetaTitle, MetaModified, SourceDoc}.
