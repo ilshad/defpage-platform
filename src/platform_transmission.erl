e0642551dec3e4dd156878d478624bc5a896d0c6
Ilshad Khabibullin <astoon.net@gmail.com>
2012-08-24 02:18:38 +0400

    introduced types and records for transmission module

--- src/platform_transmission.erl
+++ src/platform_transmission.erl
@@ -10,3 +10,26 @@
 
 -include("platform.hrl")
 
+-record(basic_auth, {username     :: string(),
+		     password     :: string()}).
+
+-record(secret_auth, {secret      :: string()}).
+
+-type(auth() :: #basic_auth{} | #secret_auth{}).
+
+-record(rest_transmission, {url   :: string(),
+			    auth  :: auth()}).
+
+-record(dirty_transmission, {url  :: string(),
+			     auth :: string()}.    
+
+-type(transmisson() :: #rest_transmission{} | #dirty_transmission).
+
+%%------------------------------------------------------------------------------
+%%
+%% Create entry (transmit document to the host fisrt time) by given document's
+%% content and transmission metadata.
+%%
+%%------------------------------------------------------------------------------
+-spec(create_entry(Transmission::#transmission{},
+		   
