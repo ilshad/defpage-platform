2be544827df8f649ebbc452f9239fed3fff6771c
Ilshad Khabibullin <astoon.net@gmail.com>
2012-08-29 03:01:06 +0400

    transmission : auth headers

--- src/platform_transmission.erl
+++ src/platform_transmission.erl
@@ -6,30 +6,48 @@
 -export([]).
 
 %% testing
--export([]).
+-export([create_entry/2]).
 
--include("platform.hrl")
+-include("platform.hrl").
 
--record(basic_auth, {username     :: string(),
-		     password     :: string()}).
-
--record(secret_auth, {secret      :: string()}).
-
+-record(basic_auth, {username :: string(), password :: string()}).
+-record(secret_auth, {secret :: string()}).
 -type(auth() :: #basic_auth{} | #secret_auth{}).
 
--record(rest_transmission, {url   :: string(),
-			    auth  :: auth()}).
-
--record(dirty_transmission, {url  :: string(),
-			     auth :: string()}.    
-
--type(transmisson() :: #rest_transmission{} | #dirty_transmission).
+-record(rest_transmission, {url :: string(), auth :: auth()}).
+-record(dirty_transmission, {url :: string(), auth :: auth()}.
+-type(transmission() :: #rest_transmission{} | #dirty_transmission{}).
 
 %%------------------------------------------------------------------------------
 %%
-%% Create entry (transmit document to the host fisrt time) by given document's
-%% content and transmission metadata.
+%% Create entry (transmit document to the host fisrt time) by given transmission
+%% metadata and documents's content. Return doc Id.
 %%
 %%------------------------------------------------------------------------------
--spec(create_entry(Transmission::#transmission{},
-		   
+-spec(create_entry(Transmission::transmission(), Content:string()) -> string()).
+
+create_entry(#rest_transmission=Transmission, Content) ->
+    Request = {Transmission#rest_transmission.url,
+	       [auth_header(Transmission#rest_transmission.auth)],
+	       "application/json",
+	       iolist_to_binary(mochijson2:encode(Fields))},
+    case httpc:request(post, Request, [], []) of
+	{ok, {{_, 201, _}, _, Body}} ->
+	    {struct, ResponseFields} = mochijson2:decode(Body),
+	    proplists:get_value(<<"id">>, ResponseFields);
+	_ -> error
+    end.
+
+%%------------------------------------------------------------------------------
+%%
+%% Create authorization HTTP header.
+%%
+%%------------------------------------------------------------------------------
+-spec(auth_header(Auth::auth()) -> string()).
+
+auth_header(#basic_auth=Auth) ->
+    LoginPass = Auth#basic_auth.username ++ ":" ++ Auth#basic_auth.password,
+    {"Authorization", "Basic " ++ binary_to_list(base64:encode(LoginPass))};
+
+auth_header(#secret_auth=Auth) ->
+    {"X-Secret", Auth#secret_auth.secret}.
