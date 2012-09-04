50f54206639b579419d2f08d112a8563574d2ffc
Ilshad Khabibullin <astoon@spacta.com>
2012-09-04 15:36:17 +0400

    first transmission function compiled ok

--- src/platform_transmission.erl
+++ src/platform_transmission.erl
@@ -6,7 +6,7 @@
 -export([]).
 
 %% testing
--export([create_entry/2]).
+-export([create_entry/2, content/3]).
 
 -include("platform.hrl").
 
@@ -15,7 +15,7 @@
 -type(auth() :: #basic_auth{} | #secret_auth{}).
 
 -record(rest_transmission, {url :: string(), auth :: auth()}).
--record(dirty_transmission, {url :: string(), auth :: auth()}.
+-record(dirty_transmission, {url :: string(), auth :: auth()}).
 -type(transmission() :: #rest_transmission{} | #dirty_transmission{}).
 
 -record(content, {title :: string(),
@@ -28,15 +28,14 @@
 %% metadata and documents's content. Return doc Id.
 %%
 %%------------------------------------------------------------------------------
--spec(create_entry(Transmission::transmission(),
-		   Content::#content{}) -> string()).
+-spec(create_entry(transmission(), #content{}) -> string()).
 
-create_entry(#rest_transmission=Transmission, Content) ->
+create_entry(#rest_transmission{url=Url, auth=Auth}, Content) ->
     ContentFields = {struct, [{<<"title">>, Content#content.title},
 			      {<<"abstract">>,  Content#content.abstract},
 			      {<<"body">>, Content#content.body}]},
-    Request = {Transmission#rest_transmission.url,
-	       [auth_header(Transmission#rest_transmission.auth)],
+    Request = {Url,
+	       [auth_header(Auth)],
 	       "application/json",
 	       iolist_to_binary(mochijson2:encode(ContentFields))},
     case httpc:request(post, Request, [], []) of
@@ -53,9 +52,23 @@ create_entry(#rest_transmission=Transmission, Content) ->
 %%------------------------------------------------------------------------------
 -spec(auth_header(Auth::auth()) -> string()).
 
-auth_header(#basic_auth=Auth) ->
-    LoginPass = Auth#basic_auth.username ++ ":" ++ Auth#basic_auth.password,
+auth_header(#basic_auth{username=Username, password=Password}) ->
+    LoginPass = Username ++ ":" ++ Password,
     {"Authorization", "Basic " ++ binary_to_list(base64:encode(LoginPass))};
 
-auth_header(#secret_auth=Auth) ->
-    {"X-Secret", Auth#secret_auth.secret}.
+auth_header(#secret_auth{secret=Secret}) ->
+    {"X-Secret", Secret}.
+
+%%------------------------------------------------------------------------------
+%%
+%% Create content record (data structure).
+%%
+%%------------------------------------------------------------------------------
+-spec(content(Title::string(),
+	      Abstract::string(),
+	      Body::string()) -> #content{}).
+
+content(Title, Abstract, Body) ->
+    #content{title = Title,
+	     abstract = Abstract,
+	     body = binary_to_list(base64:encode(Body))}.
